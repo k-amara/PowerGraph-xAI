7860e4866f733e7244aa53f44b96b84cf712b5a9
ial-amanna <l.annamalai98@gmail.com>
2023-04-05 13:51:57 +0200

    added comments

--- jobscript.sh
+++ jobscript.sh
@@ -4,11 +4,9 @@
 #SBATCH --time=04:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --job-name=all_datasets
-#SBATCH --output=logs/uk_multiclass_gat
-#SBATCH --error=error/uk_multiclass_gat
+#SBATCH --output=logs/ieee24_multiclass_gat
+#SBATCH --error=error/ieee24_multiclass_gat
 #SBATCH --gpus=rtx_3090:1
 #SBATCH --gres=gpumem:8G
 
-# /cluster/scratch/alakshmanan/miniconda3/envs/gnn/bin/python /cluster/home/alakshmanan/ra_work/all_inmem_class.py
-# /cluster/scratch/alakshmanan/miniconda3/envs/gnn/bin/python /cluster/home/alakshmanan/ra_work/InMemoryPowerGridsDatasets.py
-/cluster/scratch/alakshmanan/mininconda3/envs/gnn/bin/python /cluster/home/alakshmanan/ra_work/code/train_gnn.py --dataset_name uk --datatype multiclass --model_name gat
+/cluster/scratch/alakshmanan/mininconda3/envs/gnn/bin/python /cluster/home/alakshmanan/ra_work/code/train_gnn.py --dataset_name ieee24 --datatype multiclass --model_name gat
