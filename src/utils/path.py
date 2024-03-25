39d1b1e7d85316980c922325e1393acb64ed8735
kamara <22amara.kenza@gmail.com>
2024-03-25 11:42:49 +0100

    new

--- src/utils/path.py
+++ src/utils/path.py
@@ -2,16 +2,10 @@
 #
 import os
 
-# CKPT_ROOT = "C:/Users/avarbella/Documents/GraphGym-master/GraphGym-master/graphframex-main/"
-<<<<<<< HEAD:src/utils/path.py
 # CKPT_ROOT = "C:/Users/avarbella/Documents/GraphGym-master/powergrid_datasets/"
 CKPT_ROOT = "/cluster/work/zhang/kamara/powergraph-storage/"
 
 DATA_DIR = CKPT_ROOT + "dataset/"
-=======
-CKPT_ROOT = "./"
-DATA_DIR = CKPT_ROOT + "data/"
->>>>>>> 10bd3ae749584c526805ae68a517eb45dfe59c64:code/utils/path.py
 MODEL_DIR = CKPT_ROOT + "model/"
 LOG_DIR = CKPT_ROOT + "logs/"
 RESULT_DIR = CKPT_ROOT + "results/"
