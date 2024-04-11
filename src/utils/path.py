77820e06a7840e19a4c6b10f23da0507d3c30a0b
kamara <22amara.kenza@gmail.com>
2024-04-11 09:52:09 +0200

    pat

--- src/utils/path.py
+++ src/utils/path.py
@@ -3,7 +3,7 @@
 import os
 
 # CKPT_ROOT = "C:/Users/avarbella/Documents/GraphGym-master/powergrid_datasets/"
-CKPT_ROOT = "/cluster/work/zhang/kamara/powergraph-storage/"
+CKPT_ROOT = "/cluster/scratch/kamara/PowerGraph/"
 
 DATA_DIR = CKPT_ROOT + "dataset/"
 MODEL_DIR = CKPT_ROOT + "model/"
